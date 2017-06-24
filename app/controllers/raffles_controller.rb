287b9ce396764c83b7d7bcf31b9dc78e44bac5c5
Manuel Bustillo <mayn13@gmail.com>
2017-06-24 12:10:41 +0200

    Create raffles controller, routes and views

---
+++ app/controllers/raffles_controller.rb
@@ -0,0 +1,22 @@
+class RafflesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
