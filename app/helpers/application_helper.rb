6508869fa27eee0a2fdc4ca43356067c77b2a939
Manuel Bustillo <mayn13@gmail.com>
2017-06-25 11:49:43 +0200

    Create raffle show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,12 @@ module ApplicationHelper
     t(translation_key, model_name: t("#{model.name.downcase}.one", scope: 'activerecord.models'))
   end
 
-  def edit_model_text(model, femenine=false)
-    translation_key = "activerecord.new#{'_femenine' if femenine}"
+  def edit_model_text(model)
     t('activerecord.edit', model_name: t("#{model.name.downcase}.one", scope: 'activerecord.models'))
+  end
+
+  def show_model_text(model_instance, attribute)
+    "#{t("#{model_instance.class.name.downcase}.one", scope: 'activerecord.models')} #{model_instance[attribute]}"
   end
 
   def index_model_text(model)
@@ -35,5 +38,13 @@ module ApplicationHelper
     table = BootstrapTable.new
     yield table
     table.to_html
+  end
+
+  def bootstrap_description(instance, attributes)
+    content_tag :dl, class: 'dl-horizontal' do
+      attributes.to_a.map do |(k, v)|
+        (content_tag :dt, instance.class.human_attribute_name(k)) + (content_tag :dd, v)
+      end.inject(:+)
+    end
   end
 end
