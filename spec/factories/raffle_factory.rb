3887ba3eabba52974f601a621834076688e4e38e
Manuel Bustillo <mayn13@gmail.com>
2018-02-26 22:58:16 +0100

    Fix tests

--- spec/factories/raffle_factory.rb
+++ spec/factories/raffle_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :raffle do
-    user
+    association :owner, factory: :user
     name 'Raffle name'
     starts_on Date.new(2018, 1, 1)
     ends_on Date.new(2019, 1, 1)
