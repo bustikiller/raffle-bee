ca43f9084e5f598864990d88ded0a19817d8984e
Manuel Bustillo <mayn13@gmail.com>
2018-03-20 22:42:30 +0100

    Revert "Use left join instead of join to display sellers without sales"
    This reverts commit 8de63c7de719e281b491e9e026a90d690eaa116d.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,6 +2,6 @@ class AssignmentsController < ApplicationController
   load_and_authorize_resource :raffle
 
   def index
-    @sellers = @raffle.tickets.left_joins(:user).group('users.email').count.sort_by{|_k,v| v}.reverse
+    @sellers = @raffle.tickets.joins(:user).group('users.email').count.sort_by{|_k,v| v}.reverse
   end
 end
