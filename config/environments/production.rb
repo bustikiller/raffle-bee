579b4b034daec069ab66895951377bba2ca372ca
Bustikiller <mayn13@gmail.com>
2019-03-17 13:37:02 +0100

    Configure Sendgrid as email sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "raffle-bee_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'https://raffle-bee.herokuapp.com'}
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: "25",
+  domain: 'heroku.com',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
